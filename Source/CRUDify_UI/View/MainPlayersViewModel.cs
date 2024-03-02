62c4e07d8500e0ca1278fcdd34b55df593ec64be
Ajinkya Kulkarni <ak152122@renishaw.com>
2024-03-02 17:46:16 +0530

    #1-Binded hardcoded values to a template which will be replaced when database is connected

---
+++ Source/CRUDify_UI/View/MainPlayersViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CRUDify_UI
+{
+    class MainPlayersViewModel
+    {
+        public MainPlayersViewModel()
+        {
+            var module = new MainPlayersModule();
+            module.LastName = "CR7";
+            MainVM = module;
+
+            ListOfPlayers = new ObservableCollection<MainPlayersModule>()
+            {
+                new MainPlayersModule() {FirstName = "Ajinkya", LastName = "Kulkarni", Age = 25, IsActive= true},
+                new MainPlayersModule() {FirstName = "Prathamesh", LastName = "Vaiday", Age = 24, IsActive= true},
+                new MainPlayersModule() {FirstName = "Ravi", LastName = "Kumar", Age = 22, IsActive= true},
+                new MainPlayersModule() {FirstName = "Leo", LastName = "Messi", Age = 21, IsActive= true},
+                new MainPlayersModule() {FirstName = "Cristinao", LastName = "Ronaldo", Age = 15, IsActive= true}
+            };
+        }
+
+        public string FirstName { get; set; }
+
+        public MainPlayersModule MainVM { get; set; }
+
+        public ObservableCollection<MainPlayersModule> ListOfPlayers { get; private set; }
+    }
+}
