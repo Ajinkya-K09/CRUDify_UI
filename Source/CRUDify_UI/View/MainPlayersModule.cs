836f558eeb4937839b99f3cfdcbfbad4d7f1f66b
Ajinkya Kulkarni <133676732+Ajinkya-K09@users.noreply.github.com>
2024-03-03 13:16:12 +0530

    MERGE #1 INTO dev
    * #1-Created and UI
    
    * #1-Binded hardcoded values to a template which will be replaced when database is connected
    
    * #3 - Changes for Database connectivity
    
    ---------
    
    Co-authored-by: Ajinkya Kulkarni <ak152122@renishaw.com>

---
+++ Source/CRUDify_UI/View/MainPlayersModule.cs
@@ -0,0 +1,25 @@
+﻿using CRUDify_UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDify_UI
+{
+    class MainPlayersModule
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+    }
+}
---
+++ Source/CRUDify_UI/View/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DbConnector;
+using System.Windows;
+
+namespace CRUDify_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DatabaseConnection = new DatabaseConnection();
+        }
+
+        public DatabaseConnection DatabaseConnection { get; set; }
+    }
+}
